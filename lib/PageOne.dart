e4074bb40199bbdbcfe7b513743db1ebee161705
dido <dido.malambu@gmail.com>
2018-08-12 09:12:09 +0200

    Initial commit

---
+++ lib/PageOne.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PageOne extends StatelessWidget { // Creating a simple example page.
+  @override
+  Widget build(BuildContext context) {
+    return new Center(child: new Text("Page one"));
+  }
+}
---
+++ lib/PageThree.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PageThree extends StatelessWidget { // Creating a simple example page.
+  @override
+  Widget build(BuildContext context) {
+    return new Center(child: new Text("Page three"));
+  }
+}
